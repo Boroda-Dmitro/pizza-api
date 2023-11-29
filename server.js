0ca140506bfff3688c2da485a1fb307a6ec725e8
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-11-29 10:45:15 +0200

    add mongobd

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 import app from "./app.js";
 
-app.listen(3000, () => console.log("run"));
+import mongoose from "mongoose";
+
+// const { BD_HOST } = process.env;
+
+const BD_HOST =
+  "mongodb+srv://Dimbor:xW3aTufhkk7d3X7m@cluster0.iautypv.mongodb.net/pizza-api?retryWrites=true&w=majority";
+
+mongoose
+  .connect(BD_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 
 // http://localhost:3000/
+
+// xW3aTufhkk7d3X7m
+
